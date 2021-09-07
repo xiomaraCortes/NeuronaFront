6b0dff1caf71e15deb4199273290ce1779d59ccd
xiomaraCortes <55770691+xiomaraCortes@users.noreply.github.com>
2021-09-07 18:19:48 -0500

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import {Component, Input, OnInit} from '@angular/core';
+import {NetworkMachineService} from './services/network-machine.service';
+import {MachineModel} from './model/MachineModel';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'netowrk-machine';
+  input: string;
+  machine: MachineModel;
+
+  constructor(private networkMachineService: NetworkMachineService) { }
+
+  ngOnInit(): void {
+  }
+
+
+  keyup(text: string): void {
+    this.input = text;
+  }
+
+  clickItem(): void  {
+    this.networkMachineService.getMachineNetwork(this.input.split(',')).subscribe(result => {
+       console.log(result);
+       this.machine = result.data;
+    }, error => {
+      console.log(error);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {CommunicatorService} from './services/communicator.service';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [CommunicatorService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
