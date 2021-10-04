8abdefda1988b2794be22e9ef71096eb4a8acf30
Xiomara <xiomaracortes0@gmail.com>
2021-10-03 20:53:24 -0500

    Update the code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,25 +34,8 @@ export class AppComponent implements OnInit {
   }
 
   clickItem(): void  {
-    this.information = '';
-    if(!this.checkInputIsValid()) {
-       this.information = 'Por favor revise la data ingresada, recuerde que debe ser  maximo dos campos y minimo, adicional deben ser mayor a 0  y menor 2';
-       return;
-    }
 
-    if(!this.checkFieldIsValid(this.error)) {
-      this.information = 'En el campo error debe ingresar un campo numerico';
-      return;
-   }
-
-   
-   if(!this.checkFieldIsValid(this.coeficiente)) {
-     this.information = 'En el campo factor de aprendizaje debe ingresar un campo numerico';
-     return;
-   }
-
-
-    this.networkMachineService.getMachineNetwork(this.input.split(';'), Number(this.error), Number(this.coeficiente)).subscribe(result => {
+    this.networkMachineService.getMachineNetwork(this.input.split(';'), 0, 0).subscribe(result => {
        this.machine = result.data;
     }, error => {
       this.information  = JSON.stringify(error);
